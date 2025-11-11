dbd19910291dca153b6cb914e2cf987fab917e80
BounzieBanana <luke.jamis@gmail.com>
2025-11-11 21:51:57 +0800

    updated calculator.py with add

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+
 a = float(input("Enter first number: "))
 b = float(input("Enter second number: "))
 
@@ -7,4 +8,17 @@ print("2. Subtraction (-)")
 print("3. Multiplication (*)")
 print("4. Division (/)")
 
+
+
+
+
+def addition(a, b):
+
+    return a + b
+
 operation = input("Choose what operation you want to do\n")
+
+
+
+
+
