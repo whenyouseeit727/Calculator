de35d5fc34973144d7df5c4b7d32065da18168ce
esclamadofrancereal-sys <esclamadofrancereal@gmail.com>
2025-11-11 21:16:40 +0800

    Update calculator.py
    initial structure

---
+++ calculator.py
@@ -0,0 +1,10 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Select an operation:")
+print("1. Addition (+)")
+print("2. Subtraction (-)")
+print("3. Multiplication (*)")
+print("4. Division (/)")
+
+operation = input("Choose what operation you want to do\n")
