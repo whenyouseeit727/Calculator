baff0efc2966508ca2f9fb739aeef49c3db90a06
esclamadofrancereal-sys <esclamadofrancereal@gmail.com>
2025-11-11 21:33:52 +0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,17 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Select an operation:")
+print("1. Addition (+)")
+print("2. Subtraction (-)")
+print("3. Multiplication (*)")
+print("4. Division (/)")
+
+operation = input("Choose what operation you want to do\n")
+
+
 def multiplication_numbers(a, b):
 
     return a * b
 
-result = multiplication_numbers(10, 4)
-print("Result:", result)  
+
