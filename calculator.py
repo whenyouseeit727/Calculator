e34341f7dc4ba1e7aec976daea8dcf620dba652a
Jagitr <jg.tumanda@gmail.com>
2025-11-11 22:51:03 +0800

    added operation selector

--- calculator.py
+++ calculator.py
@@ -8,24 +8,34 @@ print("2. Subtraction (-)")
 print("3. Multiplication (*)")
 print("4. Division (/)")
 
-
-
+operation = input("Choose what operation you want to do\n")
 
 
 def subtraction(a, b):
     return a - b
 def addition(a, b):
     return a + b
-def division(a,b):
+def division(a, b):
     return a/b
-    
 def multiply(a, b):
     return a*b
 
+if operation == "1":
+    result = addition(a,b)
+    print(result)
+elif operation == "2":
+    result=subtraction(a,b)
+    print(result)
+elif operation == "3":
+    result=multiply(a,b)
+    print(result)
+elif operation == "4":
+    result=division(a,b)
+    print(result)
 
 
 
-operation = input("Choose what operation you want to do\n")
+
 
 
 
